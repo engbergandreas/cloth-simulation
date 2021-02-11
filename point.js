d1306bf64b78b9666137c705da54d87e24f595cc
Andreas <adde.engberg@gmail.com>
2021-02-11 14:57:18 +0100

    Typ fungerande fake euler
    4masses med fake euler ser okeeej ish ut

--- point.js
+++ point.js
@@ -1,51 +1,64 @@
 class Point {
-    constructor(x,y,r = 20) {
-        this.mass = 2;
+    constructor(x,y,r = 10, color)  {
+        this.mass = 1; //kg
         this.pos = createVector(x, y);
-        this.vel = createVector();
-        this.acc = createVector();
+        this.vel = createVector(0,0); // m per s squared
+        this.acc = createVector(0,0); //m per s
         this.radius = r;
+        this.c = color;
 
         //bindings between
-        this.k = 0.1;
-        this.b = 1;
-        this.L0 = createVector(0, ); //spring at rest
+        this.k = 0.2; //700
+        this.b = 1.5; //2
+        this.L0 = 50; //spring at rest
         this.neighbors = [];
 
         //Temporary
         this.positionvalues = []; // Logga värden?
-    }
 
+        this.log = false;
+    }
+    
     calculateInternalForce() {
         let sum = createVector();
         for(let i = 0; i < this.neighbors.length; i++) {
             //spring force calculations
             let springForce = createVector();
             //p1p2  => p2 - p1 
-            let L = p5.Vector.sub(this.neighbors[i].pos, this.pos);
-            if(L.x == 0 && L.y == 0) { //potential bug, check for epsilon
+            let L = p5.Vector.sub(this.pos,this.neighbors[i].pos);
+            let length = Math.sqrt(L.x*L.x + L.y*L.y);
+
+            if(length <= 0.000001) { //potential bug, check for epsilon
                 springForce.mult(0); // => force = [0;0]
             }
             else {
-                let dist = p5.Vector.sub(abs(L),this.L0); // Måste göra p5.vector
                 let normalized = L.normalize();
-                springForce.x = this.k*dist.x*normalized.x;
-                springForce.y = this.k*dist.y*normalized.y;
+                
+                let displacement = length - this.L0;
+
+                springForce.x = this.k * displacement*normalized.x;
+                springForce.y = this.k * displacement*normalized.y;
+
+                // let absL = createVector(abs(L.x), abs(L.y), abs(L.z))
+                // let dist = p5.Vector.sub(absL,this.L0); // Måste göra p5.vector
+               
+                // springForce.x = this.k*dist.x*normalized.x;
+                // springForce.y = this.k*dist.y*normalized.y;
             }
             sum.add(springForce);
-            //console.log("springForce: i", i, springForce);
+            this.log ? console.log("springForce: i", i, springForce) : 0;
             //force.add(this.k * (L.sub(this.L0)) * L / L.mag());
 
             //damping forces calc
             let dampForce = p5.Vector.sub(this.vel, this.neighbors[i].vel);
             dampForce.mult(this.b);
-            //console.log("dampForce: i", i, dampForce);
+            this.log ? console.log("dampForce: i", i, dampForce) : 0;
 
             sum.add(dampForce);
         }
-        //return -force ? 
-        //console.log("summa : ", sum)
+
         sum.mult(-1);
+        this.log ? console.log("summa : ", sum) : 0 ;
         return sum;
     }
 
@@ -55,11 +68,11 @@ class Point {
         }
     }
 
-    eulerIntegration(x, dx){
-        const h = 0.05;
+    eulerIntegration(x, xDerivate){
+        const h = 0.1;
         //timestep h = 1 frame?
-        dx.mult(h);
-        let y = x.add(dx); //h??
+        xDerivate.mult(h);
+        let y = x.add(xDerivate); //h??
         return y;
     }
     calcForce () {
@@ -67,29 +80,46 @@ class Point {
     }
     update() {
         this.render();
-        this.acc.set(1/this.mass,1/this.mass); 
+        //this.acc.set(1/this.mass,1/this.mass); 
         let Fint = this.calculateInternalForce();
-        //Fint.mult(0);
         const Fg = createVector(0,this.mass * 9.82);
+        
         const F = createVector(0.0, 0.0);
+
         //a1(:,t + 1) = 1 / m * (-Fint(:,1) + F + Fg);
         let forceSum = p5.Vector.add(Fint,F);
+        this.log ? console.log("Fint + F", forceSum) : 0;
         forceSum.add(Fg);
+        if(this.log){
+        console.log("Force gravity", Fg)
+        console.log("Fint + F + Fg", forceSum);
+        }
         //console.log("forcex: x", forceSum.x);
         //console.log("forcex: y", forceSum.y);
         //console.log("force sum", forceSum);
 
-        this.acc.x = forceSum.x;
-        this.acc.y = forceSum.y;
+        this.acc.x = 1/this.mass * forceSum.x;
+        this.acc.y = 1/this.mass * forceSum.y;
 
-        this.vel = this.eulerIntegration(this.vel, this.acc);
-        this.pos = this.eulerIntegration(this.pos, this.vel);
+        let h = 0.01;
+        this.acc.mult(h);
+        this.vel.add(this.acc); //this.vel = this.vel + this.acc*h
 
+        //this.vel.mult(h); //FUNKA DÅÅÅEÅEÅEÅE :(
+        this.pos.add(this.vel);
+
+
+        //this.vel = this.eulerIntegration(this.vel, this.acc);
+        //this.pos = this.eulerIntegration(this.pos, this.vel);
         //this.positionvalues.add([this.pos.x, this.pos.y]); // Logga värden?
-        
-        //console.log("position: ", this.pos);
-        //console.log("velocity: ", this.pos);
+        if(this.log) {
+        console.log("acceleration: ", this.acc);
+        console.log("velocity: ", this.vel);
+        console.log("position: ", this.pos);
+        }
 
+
+        this.render();
     }
 
     drawLine(p2) {
@@ -97,7 +127,37 @@ class Point {
     }
     
     render() {
-        fill(1);
+        fill(this.c);
+        for(let n in this.neighbors) {
+            this.drawLine(this.neighbors[n]);
+        }
         ellipse(this.pos.x, this.pos.y, this.radius);
+
+
     }
+
+    renderHelperArrows() {
+        const b = createVector(400,400);
+        let t = 'velocity';
+        textSize(15)
+        text(t, 300, 400)
+        this.drawArrow(b, this.vel.copy(), 0);
+        t = "acceleration"
+        text(t, 550, 400)
+        this.drawArrow(b.add(100), this.acc.copy(), 0);
+    }
+
+    drawArrow(base, vec, myColor) {
+        push();
+        stroke(myColor);
+        strokeWeight(3);
+        fill(myColor);
+        translate(base.x, base.y);
+        line(0, 0, vec.x, vec.y);
+        rotate(vec.heading());
+        let arrowSize = 7;
+        translate(vec.mag() - arrowSize, 0);
+        triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
+        pop();
+      }
 }
--- sketch.js
+++ sketch.js
@@ -1,42 +1,79 @@
-let p1;
+let p1, p2, p3, p4;
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	pstatic1 = new Point(100, 100, 10);
-	pstatic2 = new Point(100, 500, 10);
+	createCanvas(1200, 900);
+	pstatic1 = new Point(width/2, 100, 5, color('black'));
+	pstatic2 = new Point(width/2 + 100, 100, 5, color('black'));
 
-	p1 = new Point(500, 100);
+	// pstatic1 = new Point(100, height/2-100, 10);
+	// pstatic2 = new Point(100, height/2, 10);
+	
 
 
-	p1.addNeighbors([pstatic1, pstatic2, p2]);
-	p2.addNeighbors([pstatic1, pstatic2, p1]);
+	p1 = new Point(width/2, 300, 10, color('magenta'));
+	p2 = new Point(width/2, 400, 10, color('green'));
+	p3 = new Point(width/2 + 200, 300, 10, color('red'));
+	p4 = new Point(width/2 + 200, 400, 10, color('blue'));
 
+	// p1 = new Point(300, height/2 -100);
+	// p2 = new Point(400, height/2);
+	// p3 = new Point(350, height/2 - 100);
+	// p4 = new Point(450, height/2);
+
+
+	p1.addNeighbors([pstatic1, p2, p3, p4]);
+	p2.addNeighbors([p1, p4, p3]);
+	p3.addNeighbors([pstatic2, p1,p3, p4]);
+	p4.addNeighbors([p2, p3, p1]);
+
+	console.log("pstatic:", pstatic1.pos);
+	console.log("p1",p1.pos);
+	console.log("width", width)
+	console.log("height",height)
 }
 
 // THE PROBLEMS: SKITEN FUNKAR EJ
 // THE PLAN: Jämför mot matlab på något bra sätt?
 // 
 
-function simulate() {
-	const h = 0.01;
-	const endTime = 10;
-	let timeSteps =[];
-	let count = 0;
-	
-	for(let t= 0; t < endTime; t+= h)	{
-		timeSteps[count] = t;
-		count++;
+function mousePressed() {
+	for(let i = 0; i < 50; i++) {
+	p1.update();
+	p2.update();
+	p3.update();
+	p4.update();
 	}
-	console.log(timeSteps);
 }
 
 function draw() {
-	background(255)
-	line(100, 100, 100, 700);
-	//fill(255);
-	p1.update();
+	background(230)
+	line(0, 100, width, 100);
+	line(100, 50, 100, height);
+
 	pstatic1.render();
+	pstatic2.render();
+	//p1.renderHelperArrows();
+	
+
+
 	pstatic1.drawLine(p1);
+	pstatic2.drawLine(p3);
+	// p1.drawLine(p2);
+	// p3.drawLine(p1);
+	// p3.drawLine(p2);
+	// p4.drawLine(p2);
+	// p4.drawLine(p3);
+	// p4.drawLine(p1);
+
+	p1.update();
+	p2.update();
+	p3.update();
+	p4.update();
+
+	p1.render();
+	p2.render();
+	p3.render();
+	p4.render();
 }
 
 
